2927c66cccba7d6b15e6fe6646b0eb46c0535413
Alberto Crespo <albertocresposanchez@gmail.com>
2019-07-06 23:23:06 +0200

    Finish request params directives

--- router/http/src/main/scala/com/acs/scala/server/mad/router/directives/RequestDirectives.scala
+++ router/http/src/main/scala/com/acs/scala/server/mad/router/directives/RequestDirectives.scala
@@ -5,31 +5,6 @@ import com.acs.scala.server.mad.router.convertions.{BodyReader, DefaultFormats, 
 import com.acs.scala.server.mad.router.exception.UnexpectedContentTypeException
 import com.acs.scala.server.mad.router.model.Response
 
-object Main {
-  def main(args: Array[String]): Unit = {
-
-    (1 to 15).foreach { i =>
-      var generics = List[String]()
-      var params = List[String]()
-      var action = List[String]()
-      var result = List[String]()
-
-      (1 to i).foreach { k =>
-        generics = generics ++ List(s"P$k, R$k")
-        params = params ++ List(s"param$k: Param[P$k, R$k]")
-        action = action ++ List(s"R$k")
-        result = result ++ List(s"param$k.pathValue")
-      }
-
-      println(
-        s"""def requestParam[${generics.mkString(",")}](${params.mkString(",")})(action: (${action.mkString(",")}) => Response)(implicit context: RequestContext): Response = {
-           |    action(${result.mkString(",")})
-           |}
-         """.stripMargin)
-    }
-  }
-}
-
 trait RequestDirectives extends DefaultParamHandling with DefaultFormats with RequestParamsDirectives with RequestHeaderDirectives with RequestQueryDirectives {
 
   def requestBody(action: Array[Byte] => Response)(implicit context: RequestContext): Response = action(context.request.bodyBytes)
