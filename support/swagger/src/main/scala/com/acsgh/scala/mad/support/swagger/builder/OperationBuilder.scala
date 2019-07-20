fcae2d0ccc0b3e5b91aedfd5dcd6059a95b8aa59
Alberto Crespo <albertocresposanchez@gmail.com>
2019-07-20 20:09:48 +0200

    Remove old swagger aproach

--- support/swagger/src/main/scala/com/acsgh/scala/mad/support/swagger/builder/OperationBuilder.scala
+++
@@ -1,8 +0,0 @@
-package com.acsgh.scala.mad.support.swagger.builder
-
-import io.swagger.v3.oas.models.Operation
-
-case class OperationBuilder(parent: OpenApiBuilder, protected val delegate: Operation = new Operation()) extends Builder[OpenApiBuilder, Operation] {
-
-
-}
